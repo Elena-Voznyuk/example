2f8cbadce983ae3a0ef89791017702370d0c650c
Elena-Voznyuk <129968770+Elena-Voznyuk@users.noreply.github.com>
2023-05-22 18:08:46 +0300

    Update Program.cs
    add new changes

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ int [] CreatNewArray (int size, int min, int max)
     int [] array = new int[size];
     
     for (int i = 0; i < size; i++)
-        array[i] = new Random().Next(min, max);
+        array[i] = new Random().Next(min, max + 1);
     return array;
 }
 void ShowArray (int [] array)
